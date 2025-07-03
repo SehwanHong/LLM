e7ca671631d23f6fa3ddf9a1a458c78b1e6b6a35
Sehwan Hong <samhong657@gmail.com>
2025-07-03 16:55:31 +0900

    새로운 transformer_05.py 및 04_transformers.sh 파일 추가
    - Hugging Face Transformers를 사용하여 ELI5 데이터셋을 처리하고 훈련하는 코드 구현
    - 데이터 전처리, 토큰화, 텍스트 그룹화 및 모델 훈련 과정 포함
    - SLURM 스크립트를 통해 단일 GPU 환경에서 실행할 수 있도록 설정

---
+++ python_codes/transformer_05.py
@@ -0,0 +1,127 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from transformers import pipeline
+from transformers import DataCollatorForLanguageModeling
+from transformers import TrainingArguments
+from transformers import Trainer
+
+import torch
+
+import math
+
+if __name__ == "__main__": 
+    model = AutoModelForCausalLM.from_pretrained("distilbert/distilgpt2", torch_dtype="auto")
+    tokenizer = AutoTokenizer.from_pretrained("distilbert/distilgpt2")
+    eli5 = load_dataset("eli5_category", split="train[:5000]")
+
+    eli5 = eli5.train_test_split(test_size=0.2)
+    
+    print(f"[DEBUG] ELI5-Category Dataset load")
+
+    print(f'{eli5["train"][0]}')
+
+    print("-" * 64)
+
+    def preprocess_function(examples): 
+        return tokenizer([" ".join(x) for x in examples["answers.text"]])
+
+    eli5 = eli5.flatten()
+
+    print(f"[DEBUG] ELI-5 Flatten")
+
+    print(f"eli5 flatten = {eli5}")
+
+    print("-" * 64)
+    
+    tokenized_eli5 = eli5.map(
+        preprocess_function,
+        batched=True,
+        num_proc=4,
+        remove_columns=eli5["train"].column_names,
+    )  
+
+    print(f"[DEBUG] Tokenized ELI5-Category Dataset load")
+
+    print(f'{tokenized_eli5["train"][0]}')
+
+    print("-" * 64)
+
+    block_size = 128
+
+    def group_texts(examples):
+        # Concatenate all texts.
+        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
+        if total_length >= block_size:
+            total_length = (total_length // block_size) * block_size
+        # Split by chunks of block_size.
+        result = {
+            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+    
+    lm_dataset = tokenized_eli5.map(
+        group_texts,
+        batched=True,
+        num_proc=4,
+    )
+
+    print(f"[DEBUG] Grouped ELI5-Category Dataset load")
+
+    print(f'{lm_dataset["train"][0]}')
+
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    print(f"[DEBUG] DataCollatorForLanguageModeling load")
+
+    print(f'{data_collator([lm_dataset["train"][i] for i in range(2)])}')
+
+    print("-" * 64)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+
+    for i in range(10):
+        print(f"[DEBUG] Training {i} epoch")
+
+        model.train()
+
+        for batch in data_collator(lm_dataset["train"]):
+            outputs = model(**batch)
+            loss = outputs.loss
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        model.eval()
+
+        with torch.no_grad():
+            for batch in data_collator(lm_dataset["test"]):
+                outputs = model(**batch)
+                loss = outputs.loss 
+                print(f"Loss: {loss.item()}")
+
+    print("-" * 64)
+
+    print("[DEBUG] Text generation by pipeline")
+    prompt = "Somatic hypermutation allows the immune system to"
+    generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+    print(generator(prompt, max_new_tokens=50))
+
+    print("-" * 64)
+
+    print("[DEBUG] Text generation step by step")
+    inputs = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
+    print(f"[DEBUG] Inputs: {inputs}")
+
+    outputs = model.generate(inputs, max_new_tokens=50)
+    print(f"[DEBUG] Outputs: {outputs}")
+
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+    print("-" * 64)
---
+++ scripts/04_transformers.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+## Job Settings
+#SBATCH --job-name=transformers
+#SBATCH --time=99:00:00
+#SBATCH --partition=hopper
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64gb
+#SBATCH --comment="LLM STUDY - Transformers"
+
+## Output Settings
+#SBATCH --output=./logs/transformers_%A.log
+
+## Container Settings
+#SBATCH --container-image=/purestorage/AILAB/AI_1/shhong/enroot_images/pytorch_2_5_1.sqsh
+#SBATCH --container-mounts=/purestorage/:/purestorage/,/purestorage/AILAB/AI_1/shhong/cache:/home/$USER/.cache
+#SBATCH --no-container-mount-home
+#SBATCH --container-writable
+#SBATCH --container-workdir=/purestorage/AILAB/AI_1/shhong/LLM/python_codes
+
+## Run
+pip install -r requirements.txt
+python transformer_05.py
